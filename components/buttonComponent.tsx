24ad011e3a6f78a6bda6c7488011856f919ea547
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-02-22 12:47:31 -0300

    add some animations to buttonComponent

--- components/buttonComponent.tsx
+++ components/buttonComponent.tsx
@@ -17,8 +17,8 @@ export default function ButtonComponent({
   isLogin,
 }: IButtonComponent) {
   return (
-    <Link href={href}>
-      <Button className="w-full min-w-64 gap-2 rounded-lg text-2xl font-light ">
+    <Link className="group" href={href}>
+      <Button className="w-full min-w-64 gap-2 rounded-lg text-2xl font-light transition-all duration-300 group-hover:scale-105 ">
         {isLogin && <GithubIcon className=" fill-white" />}
         {text}
       </Button>
